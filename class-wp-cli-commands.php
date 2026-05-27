b23dbff0573fdd2f56edbeccd628ac012706fb96
Krupal Panchal <krupalpanchal1506@gmail.com>
2026-05-27 11:59:11 +0530

    Update class-wp-cli-commands.php

--- class-wp-cli-commands.php
+++ class-wp-cli-commands.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Register of all WP Commands
+ * 
+ * License: GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @author Krupal Panchal
  */
